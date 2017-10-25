beaf38cd2e33256c45c6ca2cd2321e835edd4c94
Benjamin Soung <soungbenjamin@gmail.com>
2017-10-25 13:14:26 -0400

    able to update ants using algo

--- src/components/AntList.js
+++ src/components/AntList.js
@@ -3,6 +3,8 @@ import { graphql, createFragmentContainer } from "react-relay";
 import Ant from "./Ant";
 import _ from "lodash";
 
+import generateAntWinLikelihoodCalculator from "../utils/calculateWinningOdds";
+
 class AntList extends Component {
   state = {
     ants: []
@@ -12,8 +14,33 @@ class AntList extends Component {
     return [...arr.slice(0, index), item, ...arr.slice(index + 1)];
   };
 
+  calculateAnt = ant => {
+    generateAntWinLikelihoodCalculator()(result => {
+      const newAnt = { ...ant, odds: Math.floor(result * 100) };
+      this.updateAnt(newAnt);
+    });
+  };
+
+  updateAnt = ant => {
+    const antIndex = this.state.ants.findIndex(a => a.name === ant.name);
+    const newAnts = this.arrUpdateItemByIndex(this.state.ants, antIndex, ant);
+
+    this.setState({
+      ants: newAnts.sort((a, b) => a.odds < b.odds)
+    });
+  };
+
+  onClickCalculateOdds = () => {
+    _.each(this.state.ants, this.calculateAnt);
+  };
+
   render() {
-    return <div>{_.map(this.props.ants, ant => <Ant {...this.props} />)}</div>;
+    return (
+      <div>
+        <button>calculate</button>
+        {_.map(this.props.ants, ant => <Ant {...this.props} />)}
+      </div>
+    );
   }
 }
 
