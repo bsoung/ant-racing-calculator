e1c261c5ef79ffe5c80fcc41c6a052d186f19cff
Benjamin Soung <soungbenjamin@gmail.com>
2017-10-25 12:41:39 -0400

    set up basic required components

---
+++ src/components/AntList.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Ant from "./Ant";
+
+class AntList extends Component {
+  render() {
+    return (
+      <div>
+        AntList Component
+        <Ant />
+      </div>
+    );
+  }
+}
+
+export default AntList;
---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import AntList from "./AntList";
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				Main app
+				<AntList />
+			</div>
+		);
+	}
+}
+
+export default App;
